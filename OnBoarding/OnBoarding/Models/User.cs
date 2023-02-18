af204705d4488d5ba8d1f5be55123c97b91f39e5
glekovkirilll <glekovkirill2016@gmail.com>
2023-02-18 15:56:19 +0700

    add users list

---
+++ OnBoarding/OnBoarding/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace OnBoarding.Models
+{
+    public class User
+    {
+        public String Id { get; set; }
+        public String EMail { get; set; }
+    }
+}
---
+++ OnBoarding/OnBoarding/Models/UserList.cs
@@ -0,0 +1,7 @@
+﻿namespace OnBoarding.Models
+{
+    public class UserList
+    {
+        public List<User> users { get; set; }
+    }
+}
