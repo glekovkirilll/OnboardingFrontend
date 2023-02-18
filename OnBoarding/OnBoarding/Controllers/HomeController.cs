c6c8426ad74c952d3f4dc3a0278f72400ba7dfa7
Rina Trofimova <lidesrose@gmail.com>
2023-02-18 10:47:59 +0700

    Модалочка для входа

--- OnBoarding/OnBoarding/Controllers/HomeController.cs
+++ OnBoarding/OnBoarding/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace OnBoarding.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult LogIn()
         {
             return View();
         }
