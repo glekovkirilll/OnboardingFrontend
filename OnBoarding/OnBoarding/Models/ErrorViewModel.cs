e97b4f4262f05a4b4cd23aaf34c8d92b395600e5
Rina Trofimova <lidesrose@gmail.com>
2023-02-18 10:12:57 +0700

    насилие над здравым смыслом

---
+++ OnBoarding/OnBoarding/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnBoarding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
